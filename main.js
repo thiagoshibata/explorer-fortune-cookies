f047e7f5afbb15c7689d8ea0a2305e5c878e8c34
thiago-shibata <thiago.shibatta@gmail.com>
2024-04-23 10:50:53 -0300

    adicionando comentários no main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const luck = document.querySelector(".fortune")
 const linkOpenFortuneCookie = document.querySelector("#linkOpenFortuneCookie")
 const btnAgain = document.querySelector("#btnAgain")
 let numberFortune
+//variável que vai receber a frase anterior. Iniciando com -1 para nunca iniciar igual a atual.
 let numberOld = -1
 // frases do dia
 let phrases = [
@@ -11,7 +12,6 @@ let phrases = [
   "Seu chefe vai te pedir pra ficar até mais tarde com trabalho extra",
   "O pneu do carro pode furar e estará chovendo",
 ]
-//obtendo o número de frases da sorte e subtraindo -1 para ajustar a posição 0 do array.
 
 // -- EVENTOS
 linkOpenFortuneCookie.addEventListener("click", handleOpenFortuneCookies)
@@ -22,6 +22,7 @@ function handleOpenFortuneCookies() {
   //obtendo número da sorte através do Math.random()
   numberFortune = Math.round(Math.random() * (phrases.length - 1))
 
+  //sorteando uma nova frase caso a atual seja igual a anterior.
   while (numberFortune == numberOld) {
     numberFortune = Math.round(Math.random() * (phrases.length - 1))
   }
